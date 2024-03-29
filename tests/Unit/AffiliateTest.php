fd9aa84061537eb4239b032fba052126c065b9ea
Cilkotron <sanjabudic2907@gmail.com>
2024-03-29 16:30:28 +0100

    Fixing test method

--- tests/Unit/AffiliateTest.php
+++ tests/Unit/AffiliateTest.php
@@ -4,33 +4,21 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\Affiliate;
-use Illuminate\Support\Facades\Config;
-use App\Traits\FileRead; 
-use App\Traits\Distance; 
-use App\Traits\ParseAffiliates;
 
 class AffiliateTest extends TestCase
 {
-    use FileRead, Distance, ParseAffiliates;
     public function testGetAffiliates()
     {
-        // Call the method
+      
         $affiliates = Affiliate::getAffiliates();
 
-
-        // Assert the returned value
         $this->assertIsArray($affiliates);
-        $this->assertCount(16, $affiliates);
-    
+        $this->assertCount(2, $affiliates);
 
-        // Assert the structure of each affiliate
         $this->assertArrayHasKey('affiliate_id', $affiliates[0]);
         $this->assertArrayHasKey('name', $affiliates[0]);
         $this->assertArrayHasKey('latitude', $affiliates[0]);
         $this->assertArrayHasKey('longitude', $affiliates[0]);
-        $this->assertArrayHasKey('distance', $affiliates[0]); 
 
-        // Clean up the test file
-        unlink('test_affiliates.txt');
     }
 }
