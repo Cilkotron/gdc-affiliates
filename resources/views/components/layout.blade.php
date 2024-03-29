1122654440f5b378a0a9641f39fef140d81cd9e0
Cilkotron <sanjabudic2907@gmail.com>
2024-03-29 18:03:56 +0100

    Formating test method

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,6 @@
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
-                        <i class="fas fa-user"></i>
                         <div class="flex-shrink-0">
                             <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                                 alt="Your Company">
--- tests/Unit/AffiliateTest.php
+++ tests/Unit/AffiliateTest.php
@@ -4,21 +4,23 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\Affiliate;
+use Mockery;
+use Mockery\MockInterface;
+
 
 class AffiliateTest extends TestCase
 {
     public function testGetAffiliates()
     {
-      
+
         $affiliates = Affiliate::getAffiliates();
 
         $this->assertIsArray($affiliates);
-        $this->assertCount(2, $affiliates);
+        $this->assertCount(16, $affiliates);
 
         $this->assertArrayHasKey('affiliate_id', $affiliates[0]);
         $this->assertArrayHasKey('name', $affiliates[0]);
         $this->assertArrayHasKey('latitude', $affiliates[0]);
         $this->assertArrayHasKey('longitude', $affiliates[0]);
-
     }
 }
